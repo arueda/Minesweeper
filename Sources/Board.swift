4810338a063d423c604638b0d030f76c8291b72b
Angel Rueda <angel.rueda@mobica.com>
2017-07-23 22:35:24 -0700

    Formating board file

--- Sources/Board.swift
+++ Sources/Board.swift
@@ -20,21 +20,21 @@ class Board {
         COLS = cols
         
         var placedMines = 0
-		let maxMines = 10
+        let maxMines = 10
 		
-		let mineGenerator = MineGenerator(rows*cols)
+        let mineGenerator = MineGenerator(rows*cols)
 		
-		while placedMines < maxMines {
+        while placedMines < maxMines {
 
-    		let mine = mineGenerator.getMine()
-		    //placeMine will be false if the spot is already used
-		    if placeMine(mine) {
- 		       placedMines += 1
-		    }
-		}
+            let mine = mineGenerator.getMine()
+            //placeMine will be false if the spot is already used
+            if placeMine(mine) {
+                placedMines += 1
+            }
+        }
     }
 
-	func placeMine(_ mine:Int) -> Bool{
+    func placeMine(_ mine:Int) -> Bool{
         
         var minePlaced = false 
         
@@ -47,7 +47,7 @@ class Board {
         return minePlaced
     }
 	
-	func fillNeighbors(_ mine:Int) {
+    func fillNeighbors(_ mine:Int) {
      
         let col = mine % COLS;
         let row = mine / COLS;
@@ -64,56 +64,54 @@ class Board {
         updateNeighbor(col+1,row+1)
     }
 	
-	func updateNeighbor(_ col:Int,_ row:Int){
-        if valid( col, row ) && 
-		data [(row * COLS) +  col] != MineCell {
+    func updateNeighbor(_ col:Int,_ row:Int){
+        if valid( col, row ) &&  data [(row * COLS) +  col] != MineCell {
             data [(row * COLS) +  col] += 1
         }
     }
     
     func valid(_ col:Int, _ row:Int)->Bool{
-    	return col>=0 && col<COLS && row>=0 && row<ROWS     	
-	}
+        return col>=0 && col<COLS && row>=0 && row<ROWS     	
+    }
 	
-	func click(_ col:Int,_ row:Int){
-       	let element = getElementAt(col, row)
+    func click(_ col:Int,_ row:Int){
+        let element = getElementAt(col, row)
     
         if element == MineCell {
             setElement(MineCellPressed, col, row)
             listener.minePressed()
         }else if  element >= MineCell {
-       		expansion(col, row)
-       	}
+            expansion(col, row)
+        }
     }
 
-	func boardPrint(){
+    func boardPrint(){
 		
-		var newline = 0
+        var newline = 0
 		
-		for element in data {
+        for element in data {
 			
-			if element >= MineCell{
+            if element >= MineCell{
                 print (" O ", terminator:"")
             }else{
-				if(element == MineCellPressed){
-					print (" X ", terminator:"")
-				}else if element == EmptyCellPressed{
+                if element == MineCellPressed {
+                    print (" X ", terminator:"")
+                }else if element == EmptyCellPressed{
                     print (" _ ", terminator:"")
                 }else{
-					print (" \(element) ", terminator:"")	
-				}
+                    print (" \(element) ", terminator:"")	
+                }
             }
 			
-			newline += 1
-			if newline == 8 {
-				print("\n")
-				newline = 0
-			}
-		}
-	}
+            newline += 1
+            if newline == 8 {
+                print("\n")
+                newline = 0
+            }
+        }
+    }
 	
-	func getElementAt(_ col:Int , _ row:Int) -> Int{
-        
+    func getElementAt(_ col:Int , _ row:Int) -> Int{
         return data [(row * COLS) +  col];
     }
     
@@ -121,19 +119,16 @@ class Board {
         if( valid(col, row)){
             data [(row * COLS) +  col] = value;
         }
-        
     }
 
-	func expansion(_ col:Int, _ row:Int){
-
+    func expansion(_ col:Int, _ row:Int){
         var q : [Int] = []
         
         q.append((row * COLS) +  col)
         
         while !q.isEmpty {
         
-            let mine = q.removeFirst()
-            
+            let mine = q.removeFirst()    
             let col = mine % COLS;
             let row = mine / COLS;
             
@@ -156,7 +151,6 @@ class Board {
                 if valid(col-1, row+1) {q.append((rowPlus1 * COLS) +  colMinus1)}
                 if valid(col,   row+1) {q.append((rowPlus1 * COLS) +  col)}
                 if valid(col+1, row+1) {q.append((rowPlus1 * COLS) +  colPlus1)}
-                
             }else if data[mine] > EmptyCell {
                 data[mine] -= 10
             }
